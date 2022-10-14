157b55bd76a99245ab0386ef77da13e0765d048a
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-10-14 14:13:11 -0700

    print all alphabet lower case then uppercas

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ *
+ * Return: 0 on success.  Error code otherwise
+ **/
+int main(void)
+{
+	char alpha;
+	int i = 0;
+
+	alpha = 'a';
+	while (i < 26)
+	{
+		putchar(alpha + i);
+		i++;
+	}
+	i=0;
+	alpha='A';
+	while (i < 26)
+	{
+		putchar(alpha + i);
+		i++;
+	}
+
+	alpha = '\n';
+	putchar(alpha);
+	return  (0);
+}
