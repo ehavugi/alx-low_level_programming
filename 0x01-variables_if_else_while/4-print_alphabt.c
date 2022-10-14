05eccbee0f49f473ed7d4509f53c2d0dadb423ff
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-10-14 14:30:27 -0700

    print alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ *
+ * Return: 0 on success.  Error code otherwise
+ **/
+int main(void)
+{
+	char alpha;
+	int i = 0;
+
+	alpha = 'a';
+	while (i < 26)
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha = alpha + 1;
+		i++;
+	}
+	alpha = '\n';
+	putchar(alpha);
+	return  (0);
+}
