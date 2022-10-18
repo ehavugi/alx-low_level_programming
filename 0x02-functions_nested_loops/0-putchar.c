6b5e3b444c105089ca74049b6549191ce4245923
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-10-18 15:17:59 -0700

    ptchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program entry
+ *
+ * Return: 0 on success.  Error code otherwise
+ **/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+
+}
