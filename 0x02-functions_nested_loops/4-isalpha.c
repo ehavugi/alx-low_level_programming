d6795c00f3b83d4c9e14a9d5c832e4186e33144a
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-10-19 08:26:57 -0700

    isalphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,15 @@
 #include <string.h>
 
 /**
- * _islower -- function entry
+ * _isalpha -- function entry
  *
  * @c : input alphabet
  *
  * Return: 1 if lower 0 otherwise.  Error code otherwise
  **/
-int  _islower(int c)
+int  _isalpha(int c)
 {
-	if ((c > 96) && (c < 123))
+	if(((c > 64) && (c < 91)) || ((c > 96) &&  (c < 123)))
 	{
 		return (1);
 	}
