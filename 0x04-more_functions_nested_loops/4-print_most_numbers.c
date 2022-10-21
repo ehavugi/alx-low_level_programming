64a7077955ce08c6c3370174b301f0503a07edb7
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-10-21 03:14:41 -0700

    added all prototypes in main for now and can select numbers to not print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- returns none.print 0 to 9
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != '4' && c != '4')
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
