853560a3d72e6badd5e116dda40602b61ff3bd2a
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-10-27 03:22:20 -0700

    Added rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string. Changes the input string
+ * @s: input string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int index = 0;
+	char interm;
+
+	/* find string length */
+	while (s[length] != '\0')
+	{
+		length += 1;
+	}
+	for (index = 0; index <= (length / 2); index++)
+	{
+		interm = s[index];
+		s[index] = s[length - 1 - index];
+		s[length - 1 - index] = interm;
+	}
+}
