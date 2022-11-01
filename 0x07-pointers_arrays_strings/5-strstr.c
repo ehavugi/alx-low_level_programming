d74496e5beb7b75f89e13b17f85cdb9630d546a6
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-01 16:00:07 -0700

    Added _strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/**
+ * _strstr - locate a substring
+ * @haystack: string to search in
+ * @needle: string to search
+ *
+ * Return: pointer to the beginning of found substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int index = 0;
+	unsigned int index2 = 0;
+	unsigned int current = 0;
+	unsigned int current2 = 0;
+	unsigned int max = 0;
+	char *out;
+
+	while (haystack[index] != '\0')
+	{
+		while (needle[index2] != '\0')
+		{
+			if (haystack[index] == needle[index2])
+			{/*printf("%d, %d, %d, %d \n", index, index2, current, current2);*/
+				if (current2 == 0)
+				{
+					current = index;
+				}
+				if (current2 == index2)
+				{
+					current2 += 1; }
+				else
+				{
+					current2 = 0; }
+				if (max  == current2)
+				{
+					out = &haystack[current];
+					return (out);
+				}
+			}
+			index2 +=  1;
+			if (needle[index2] == '\0')
+			{
+				max = index2 - 1;
+			}
+		}
+		index2 = 0;
+		index += 1;
+	}
+	return ('\0');
+}
