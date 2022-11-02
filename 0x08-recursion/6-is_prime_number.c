eafc48510dbe837efd32b7a450b1faccc8690d42
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-02 15:25:05 -0700

    added prime function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,39 @@
 /**
- * is_prime_numnber - return 1  if a prime number.
- * @n: input integeer
+ * prime - return 1  if a prime number.
+ * @x: input integer
+ * @y: input  controlled for division.
  *
  * Return: returns if is_prime.
+ */
+int prime(int x, int y)
+	{
+		if (y <= 1)
+		{
+			return (1);
+		}
+		if (y == x)
+		{
+			return (1);
+		}
+		if ((x % y) == 0)
+		{
+			return (0);
+		}
+		return (prime(x, y - 1));
+	}
+
+/**
+ * is_prime_number - return 1 if prime
+ * n: input
+ *
+ * Return: 1 if prime else 0
  */
 
 int is_prime_number(int n)
 {
-	int index = 2;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-
-	for (index = 2; index < n; index++)
-	{
-		if (n % index == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (prime(n, n - 1));
 }
