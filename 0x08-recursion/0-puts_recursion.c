1bb413b9972d2917c433abdda1cc8b3faa7f9d1c
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-02 12:11:35 -0700

    added betty compliance"

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion = recursive implementation of puts
+ * _puts_recursion - recursive implementation of puts
  * @str: string input
  *
  * Return: Nothing
@@ -11,7 +11,7 @@ void _puts_recursion(char *str)
 	if (str[0] == '\0')
 	{
 		_putchar('\n');
-		return ;
+		return ();
 	}
 	_putchar(str[0]);
 	_puts_recursion(&str[1]);
