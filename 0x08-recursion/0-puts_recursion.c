20c3b934e6f296367e4bab139c87dc44ec8afab0
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-02 12:06:10 -0700

    added main.h and _putchar

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion = recursive implementation of puts
+ * @str: string input
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *str)
+{
+	if (str[0] == '\0')
+	{
+		_putchar('\n');
+		return ;
+	}
+	_putchar(str[0]);
+	_puts_recursion(&str[1]);
+}
