2e93ecdca46be2abc4c5d129ded7ef9d39aea65f
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-04 16:38:29 -0700

    added 1-args which prints number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	char *name = argv[0];
+
+	*name = '\0';
+	printf("%d\n", argc - 1);
+	return (0);
+}
