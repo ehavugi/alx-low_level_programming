820a954fec71f4c864c566d1ab8caf50112e735b
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-07 09:52:51 -0800

    Added create_array that initializes to a char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * create_array - create an array of size size and initialized with char c
+ * @size: size of the array
+ * @c: the char to initializ  with.
+ *
+ * Return: point to array or null.
+ */
+
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	unsigned int index = 0;
+
+	str = malloc(sizeof(c) * size);
+
+	if (str == NULL)
+	{
+		return (str);
+	}
+
+	while (index < size)
+	{
+		str[index] = c;
+		index += 1;
+	}
+	return (str);
+}
+
