599adc9a387a3e178665bec69c1d60132b3da6ff
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-08 18:00:55 -0800

    fixed alloc-grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,33 +13,38 @@ int **alloc_grid(int width, int height)
 	int y = 0;
 	int **grid = (int **) malloc(height * sizeof(int *));
 
-	if (width  <= 0 || height <= 0 )
+	if (width  <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 	while (y < height)
 	{
 		grid[y] = (int *) malloc(width * sizeof(int));
-		y++;
-	}
+		if (grid[y] == NULL)
+		{
+			x = 0;
+			while (x <= y)
+			{
+				free(grid[x]);
+				x += 1;
+			}
+			free(grid);
+			return (NULL);
+		}
+		y += 1; }
 	x = 0;
-
 	if (grid == NULL)
 	{
-		return (NULL);
-	}
-
+		free(grid);
+		return (NULL); }
 	while (x < width)
 	{
 		while (y < height)
 		{
 			grid[y][x] = 0;
 
-			y++;
-		}
-		x++;
-		y = 0;
-	}
+			y += 1; }
+		x += 1;
+		y = 0; }
 	return (grid);
-
 }
