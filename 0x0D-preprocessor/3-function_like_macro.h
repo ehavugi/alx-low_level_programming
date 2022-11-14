e816e0f4d95ca73695d302708a3267f9ed6809dd
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-14 11:37:57 -0800

    made 4-sum and 3-function_macro.h bettty compliant

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
 #ifndef ABS_P
 #define ABS_P
-
-#define ABS(x) ((x < 0) ? -1*x : x)
-
+#define ABS(x) ((x < 0) ? -1 * x : x)
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,4 @@
 #ifndef SUM_P
 #define SUM_P
-
-#define SUM(x,y) (x + y)
-
+#define SUM(x, y) (x + y)
 #endif
