54a92abc8aed644fb0e81ab813908b622dd40f9b
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-14 13:15:03 -0800

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_T
+#define DOG_T
+/**
+ * struct dog - a strcture
+ *
+ * @name: name
+ * @age: age
+ * @owner:owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
