44b2be3374c623bd6c32461bc0f29348c5ec5a69
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-14 13:23:17 -0800

    made 1-init-dog.c betty complaint

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,14 @@
 #include "dog.h"
+
+/**
+ * init_dog - initializer for dog
+ * @d: dog struct to initalize
+ * @name: name of dog
+ * @age: age of dog
+ * @owner:owner
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	(*d).name = name;
