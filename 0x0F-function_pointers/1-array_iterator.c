2a366966bb1b4d666820a70c9289a509451c98da
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-16 10:02:46 -0800

    added array_iterator function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * array_iterator - array iterator
+ * @array: array input
+ * @size: number of elemements in array
+ * @action: what to do to eacch element
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t index = 0;
+
+	while (index < size)
+	{
+		action(array[index]);
+		index += 1;
+	}
+}
