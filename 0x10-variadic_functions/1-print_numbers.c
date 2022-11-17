d9477b8453922b593e01a2e99ef5e3144304acc9
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-17 04:24:00 -0800

    added print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers
+ * @separator: seperator to use
+ * @n: number of numbers
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int index;
+
+	va_start(ap, n);
+	for (index = n; index > 0; index -= 1)
+	{
+		if (index == (int) n)
+		{
+			printf("%d", va_arg(ap, int));
+		}
+		else
+		{
+			if (*separator != '\0')
+			{
+				printf("%s", separator);
+			}
+			printf("%d", va_arg(ap, int));
+		}
+	}
+	printf("\n");
+}
+
