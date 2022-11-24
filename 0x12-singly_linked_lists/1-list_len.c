3b1652572fa0961b49fcb8726c80e5d206ffc0ef
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-24 12:07:40 -0800

    added list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * list_len - return number of elements
+ * @h:input
+ *
+ * Return: number of elements
+ */
+
+size_t list_len( list_t *h)
+{
+	size_t len = 0;
+	list_t *x = h;
+	if (x != NULL)
+	{
+		len += 1;
+	}
+	while (x->next != NULL)
+	{
+		len += 1;
+		x = x->next;
+	}
+	return (len);
+}
