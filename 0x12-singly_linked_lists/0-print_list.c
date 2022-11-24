9332c9ec9edcc2768274546e318e6e46d90f09d2
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-24 11:49:03 -0800

    added print_listwq

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/*
+ * print_list - print a linked list
+ * @h: input
+ *
+ * Return: return size of the list
+ */
+
+size_t print_list(list_t *h)
+{
+	size_t index  = 0 ;
+	list_t *x = h;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	if (h->str == NULL)
+	{
+		printf("[%u] (nil)\n", h->len);
+	}
+	else
+	{
+		printf("[%u] %s\n", h->len, h->str);
+
+	}
+	
+	index += 1;
+
+	while (x->next != NULL)
+	{
+		x = x->next;
+		printf("[%u] %s\n", x->len, x->str);
+		index += 1;
+
+	}
+
+	return (index);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,11 @@
+# 0x12-singly\_linked\_lists
+
+## Mandatory
+1. 0-print\_list.c
+2. 1-list\_len.c
+3. 2-add\_node.c
+4. 3-add\_node\_end.c
+5. 4-free\_list.c
+6. 100-first.c
+7. 101-hello\_holberton.asm
+
