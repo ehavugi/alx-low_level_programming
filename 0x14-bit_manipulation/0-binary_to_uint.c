1e70e1924131b8d0b0ab216532a68e958e771a2f
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-11-30 23:15:43 -0800

    added  binary_to_uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+/**
+ * binary_to_uint  - convert binary to int
+ * @b: binary 
+ *
+ * Return: uint
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int out = 0;
+	int index = 0;
+	int len  = 0;
+	if (b == NULL)
+	{
+		return (0);
+	}
+	/* Find the length of string */
+
+	while(b[len] != '\0')
+	{
+		len += 1;
+	}
+	
+	/* Convert to int */
+
+	len = len - 1;
+	while (index <= len)
+	{
+		if (b[len-index]== '\0')
+		{
+			index += 1;
+			continue;
+		}
+		else if (b[len-index] == '1')
+		{
+			out += (1<<index);
+		}
+		else if (b[len-index] == '0')
+		{
+			index += 1;
+			continue;
+		}
+		else
+		{
+			return (0);
+		}
+		index += 1;
+	}
+	return (out);
+
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+# 0x14. C - Bit manipulation
+
+## Mandatory tasks
+[]  0-binary\_to\_uint.c
+[] 1-print\_binary.c
+[] 2-get\_bit.c
+[] 3-set\_bit.c
+[] 4-clear\_bit.c
+[]5-flip\_bits.c
