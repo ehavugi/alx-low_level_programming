63e5029eaecf2b65fa8a30355e106c2309345c0a
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-12-01 00:03:31 -0800

    added test.sh

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary  - print binary
+ * @n: int
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int len = 0;
+	unsigned int value = 0;
+	while ((n>>len) > 0)
+	{
+		len += 1;
+	}
+	while (len >= 0)
+	{
+		value = (n >> (len - 1)) - (n >> len << 1);
+		len -= 1;
+		if (value <= 1)
+		{
+			_putchar('0'+value);
+		}
+	}
+
+}
---
+++ 0x14-bit_manipulation/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "test 0"
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-binary_to_uint.c -o a
+./a
+rm ./a
+
+echo "test 1"
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-print_binary.c _putchar.c -o b
+./b
+rm b
+echo "test 2"
+
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-get_bit.c -o c
+./c
+rm c
+echo "test 3"
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-set_bit.c -o d
+./d
+rm d
+
+echo "test 4"
+ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-main.c 4-clear_bit.c -o e
+./e
+rm ./e
+
+echo "test 5"
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-flip_bits.c -o f
+./f
+rm ./f
