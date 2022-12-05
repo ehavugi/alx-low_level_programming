03f54624fa43b4014ec3887180fb84205e266e5c
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2022-12-05 13:49:30 -0800

    added create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <string.h>
+/**
+ * create_textfile - create text file
+ * @filename: the file name
+ * @text_content: string to write to file
+ *
+ * Return: number of characters read/print on stdout
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n;
+
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	n = write(fd, text_content, strlen(text_content));
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	return (1);
+}
