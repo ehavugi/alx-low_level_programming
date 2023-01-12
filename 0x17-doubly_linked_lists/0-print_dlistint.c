721dda91596a416021c840926ff430453e1171f0
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-01-11 20:32:24 -0800

    first comit on doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print list
+ * @h: head
+ * Return: size of list
+ */
+size_t print_dlistint(dlistint_t *h)
+{
+	dlistint_t *curr = h;
+	size_t n;
+
+	n = 0;
+	while (curr->prev != NULL)
+	{
+		curr  = curr->prev;
+	}
+
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->n);
+		curr = curr->next;
+		n += 1;
+	}
+	return (n);
+}
